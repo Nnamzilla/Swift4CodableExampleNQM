6f5acfd2ad42ad397d4fa9b3c3541de4a734891a
Nnamdi Massally <nqm14a@dmc-207-11.local>
2019-03-08 08:47:08 -0600

    Super tired but super done

--- Swift4CodableExampleNQM/ViewController.swift
+++ Swift4CodableExampleNQM/ViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
+struct Country: Decodable {
+    let name: String
+    let capital: String
+    let region: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let url = "http://restcountries.eu/rest/v2/all"
+        let url = "https://restcountries.eu/rest/v2/all"
         let urlObj = URL(string: url)
         
-        URLSession.shared.dataTask(with: urlObj!)
+        URLSession.shared.dataTask(with: urlObj!) {(data, response, error) in
+            
+            do {
+                var countries = try JSONDecoder().decode([Country].self, from: data!)
+                for country in countries {
+                    print(country.name + " - " + country.capital + " in " + country.region)
+                }
+            } catch {
+                print("We got an error")
+            }
+            
+        }.resume()
         
     }
 
