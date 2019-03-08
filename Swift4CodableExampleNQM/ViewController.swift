4f954f5f844ee6ac98691ca50881e3b2ede0185f
Nnamdi Massally <nqm14a@dmc-207-11.local>
2019-03-08 08:22:01 -0600

    Initial Commit

---
+++ Swift4CodableExampleNQM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift4CodableExampleNQM
+//
+//  Created by Nnamdi Massally on 3/8/19.
+//  Copyright © 2019 Nnamdi Massally. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
